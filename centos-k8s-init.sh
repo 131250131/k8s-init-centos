9269ca584aebad8870d1a456cdc11b4971a259e3
陈圣超 <chenshengchao@chenshengchaos-iMac.local>
2018-12-21 16:03:22 +0800

    first commit

---
+++ centos-k8s-init.sh
@@ -0,0 +1,81 @@
+#!/bin/sh
+
+#  centos-k8s-init.sh
+#  
+#
+#  Created by csc on 2018/12/21.
+#  
+
+yum install -y wget curl conntrack-tools vim net-tools socat ntp kmod ceph-common
+
+echo 'sync time'
+systemctl start ntpd
+systemctl enable ntpd
+
+echo 'disable selinux'
+setenforce 0
+sed -i 's/=enforcing/=disabled/g' /etc/selinux/config
+yum install gcc
+yum install kernel-devel
+
+#systemctl stop firewalld
+#systemctl disable firewalld
+
+cp ../k8s.conf /etc/sysctl.d/k8s.conf
+modprobe br_netfilter
+sysctl -p /etc/sysctl.d/k8s.conf
+
+cp ../kubernetes.repo /etc/yum.repos.d/kubernetes.repo
+yum makecache fast
+yum install -y kubelet kubeadm kubectl
+
+# 临时禁用selinux
+# 永久关闭 修改/etc/sysconfig/selinux文件设置
+sed -i 's/SELINUX=permissive/SELINUX=disabled/' /etc/sysconfig/selinux
+setenforce 0
+
+# 临时关闭swap
+# 永久关闭 注释/etc/fstab文件里swap相关的行
+swapoff -a
+sed -i '/swap/s/^/#/' /etc/fstab
+
+iptables -P FORWARD ACCEPT
+
+sysctl --system
+modprobe ip_vs
+modprobe ip_vs_rr
+modprobe ip_vs_wrr
+modprobe ip_vs_sh
+modprobe nf_conntrack_ipv4
+lsmod | grep ip_vs
+
+
+kubeadm config images pull --config kubeadm-master.config
+
+rm -rf $HOME/.kube
+mkdir -p $HOME/.kube
+sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+sudo chown $(id -u):$(id -g) $HOME/.kube/config
+
+docker pull registry.cn-shenzhen.aliyuncs.com/cp_m/flannel:v0.10.0-amd64
+docker pull registry.cn-hangzhou.aliyuncs.com/google_containers/kubernetes-dashboard-amd64:v1.10.0
+
+cp kubelet.conf /etc/sysconfig/
+
+kubeadm init --config kubeadm-master.config
+
+sh tag-sh.sh
+
+kubeadm reset -f
+
+kubeadm init --kubernetes-version=v1.13.1   --pod-network-cidr=10.244.0.0/16   --apiserver-advertise-address=172.17.8.101 --token=cbc0fd.0w7gve4y3ze9ntqj --token-ttl=0
+
+mkdir -p $HOME/.kube
+cp /etc/kubernetes/admin.conf $HOME/.kube/config
+chown $(id -u):$(id -g) $HOME/.kube/config
+
+kubectl apply -f kube-flannel.yml
+
+kubectl taint nodes Linux node-role.kubernetes.io/master-
+
+systemctl restart kubelet
